ba5836fc28e7d790c319e4752973ece5a68f5e15
kjh24871 <kjh24871@naver.com>
2021-11-20 06:34:14 +0900

    first setting

---
+++ src/test/java/KisangChoiKo/ATSWEB/AtsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package KisangChoiKo.ATSWEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
